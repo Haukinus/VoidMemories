d1cb2ac800face18ca549f633078a4aea4c4355c
Haukinus <haukinsthegenerist@gmail.com>
2025-02-22 12:34:52 -0700

    Added an enemy class and made some improvements to the collision

--- player.gd
+++ player.gd
@@ -3,13 +3,13 @@ extends CharacterBody3D
 @export var speed = 2
 
 #following tutorial for most of this, may be changed
-func _physics_process(delta):
+func _physics_process(_delta):
 	
 	if Input.is_action_pressed("move_right"):
-		rotate(Vector3(0,1,0), 0.05)
+		rotate(Vector3(0,-1,0), 0.05)
 		
 	if Input.is_action_pressed("move_left"):
-		rotate(Vector3(0,-1,0), 0.05)
+		rotate(Vector3(0,1,0), 0.05)
 		
 	if Input.is_action_pressed("move_forward") and not Input.is_action_pressed("move_back"):
 		#velocity was tricky to determine with tank controls but works now
@@ -22,3 +22,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_back") and not Input.is_action_pressed("move_forward"):
 		velocity =- Vector3(sin(rotation.y), 0, cos(rotation.y))
 		move_and_slide()
+	
+	if Input.is_action_just_released("attack"):
+		$AttackRange.disabled = 0
+		print($AttackRange.disabled)
+		Timer
